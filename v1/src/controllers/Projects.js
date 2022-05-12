f77156a30eefe134448443d8a984a80c87ead50c
Furkangunduz <frkn.gundz00@gmail.com>
2022-05-12 16:04:45 +0300

    Db insert list functions

--- v1/src/controllers/Projects.js
+++ v1/src/controllers/Projects.js
@@ -1,9 +1,24 @@
+const { insert, list } = require("../services/Project");
+const httpStatus = require("http-status");
+
 const create = (req, res) => {
-    res.status(200).send("Project create");
+    insert(req.body)
+        .then((response) => {
+            res.status(httpStatus.CREATED).send(response);
+        })
+        .catch((error) => {
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error);
+        });
 };
 
 const index = (req, res) => {
-    res.status(200).send("Project index");
+    list()
+        .then((response) => {
+            res.status(httpStatus.OK).send(response);
+        })
+        .catch((error) => {
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error);
+        });
 };
 
 module.exports = {
